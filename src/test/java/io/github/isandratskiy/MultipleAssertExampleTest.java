01ab55d7190ed51ce76e22f1b7e0fd64c779157a
isandratskiy <i.sandrackii@gmail.com>
2019-09-02 03:12:50 +0300

    changed selenide config

--- src/test/java/io/github/isandratskiy/MultipleAssertExampleTest.java
+++ src/test/java/io/github/isandratskiy/MultipleAssertExampleTest.java
@@ -3,22 +3,28 @@ package io.github.isandratskiy;
 import com.codeborne.selenide.*;
 import io.github.bonigarcia.seljup.SelenideConfiguration;
 import io.github.bonigarcia.seljup.SeleniumExtension;
-import io.github.isandratskiy.driver.ChromeDriverProvider;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Browsers.FIREFOX;
+import static com.codeborne.selenide.Condition.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SeleniumExtension.class)
 class MultipleAssertExampleTest {
     @SelenideConfiguration
-    static SelenideConfig selenideConfig = new SelenideConfig().browser(ChromeDriverProvider.class.getName());
+    static SelenideConfig selenideConfig = new SelenideConfig().browser(FIREFOX)
+            .startMaximized(true)
+            .headless(true);
 
     @Test
     void testSelenideConfig(SelenideDriver driver) {
         driver.open("https://the-internet.herokuapp.com");
-        SelenideElement element = $("");
-        Assertions.assertAll();
+        assertAll(
+                () -> assertEquals(43, driver.$$("#content > ul > li").size()),
+                () -> assertEquals(
+                        "Basic Auth (user and pass: admin)",
+                        driver.$$("#content > ul > li").findBy(text("Basic")).text()
+                ));
     }
 }
