46b5c5af7f89eec2ba75ff3835e5aa12a25c3b08
isandratskiy <i.sandrackii@gmail.com>
2019-11-09 05:15:30 +0200

    added webdriver runner set driver

--- src/main/java/io/github/isandratskiy/MainPage.java
+++ src/main/java/io/github/isandratskiy/MainPage.java
@@ -1,7 +1,6 @@
 package io.github.isandratskiy;
 
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideDriver;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.*;
@@ -9,27 +8,20 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class MainPage {
     private SelenideElement rootElement;
-    private SelenideDriver selenide;
 
-    public MainPage(SelenideDriver selenide) {
-        this.selenide = selenide;
+    public MainPage() {
         this.rootElement = element("#content.large-12");
-    }
-
-    public MainPage openLink(String url) {
-        selenide.getWebDriver().get(url);
-        return this;
     }
 
     private ElementsCollection getExampleRows() {
         return rootElement.$$(".columns ul li a");
     }
 
-    public void getAviableExampleTitle(String title) {
-        getExampleRows().filterBy(text(title)).find(visible).click();
+    public ElementsCollection getRows() {
+        return $$(".columns ul li a");
     }
 
-    public ElementsCollection getRows() {
-        return selenide.$$(".columns ul li a");
+    public void openAvailableExample(String title) {
+        getExampleRows().filterBy(text(title)).find(visible).click();
     }
 }
--- src/test/java/io/github/isandratskiy/selenium/jupiter/JupiterDockerSelenideTest.java
+++ src/test/java/io/github/isandratskiy/selenium/jupiter/JupiterDockerSelenideTest.java
@@ -9,21 +9,29 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.*;
 import static io.github.bonigarcia.seljup.BrowserType.CHROME;
 
 @ExtendWith(SeleniumExtension.class)
 class JupiterDockerSelenideTest {
-    private MainPage mainPage;
+    private static final String URL = "https://the-internet.herokuapp.com/";
 
     @BeforeEach
-    void arrange(@DockerBrowser(type = CHROME) SelenideDriver selenide) {
-        mainPage = new MainPage(selenide);
+    void arrange(@DockerBrowser(type = CHROME, version = "77.0") SelenideDriver selenide) {
+        setWebDriver(selenide.getWebDriver());
     }
 
     @Test
-    void canSelenideWithDockerInPageObject() {
-        mainPage.openLink("https://the-internet.herokuapp.com")
+    void canFindAllAvailableExamples() {
+        open(URL, MainPage.class)
                 .getRows()
                 .shouldHave(sizeGreaterThan(10));
+    }
+
+    @Test
+    void canOpenAvailableExample() {
+        open(URL, MainPage.class)
+                .openAvailableExample("Form Authentication");
     }
 }
