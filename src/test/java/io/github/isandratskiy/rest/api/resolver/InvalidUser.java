d7f01492bb41729c79ed5ed29e75692df5e70778
isandratskiy <i.sandrackii@gmail.com>
2019-11-16 21:40:11 +0200

    cleanup

--- src/test/java/io/github/isandratskiy/rest/api/resolver/InvalidUser.java
+++ src/test/java/io/github/isandratskiy/rest/api/resolver/InvalidUser.java
@@ -1,8 +1,11 @@
 package io.github.isandratskiy.rest.api.resolver;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
 public @interface InvalidUser {
 }
